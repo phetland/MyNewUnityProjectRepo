b0dd9d65fa8dd3e07b1dad1a2b1da6307f8f4442
phetland <46686189+phetland@users.noreply.github.com>
2023-09-08 21:47:40 -0500

    Added Prototype1Runthrough

---
+++ Prototype1Runthrough2/Assets/Scripts/LoseOnFall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//attach this script to the player
+public class LoseOnFall : MonoBehaviour
+{
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -1)
+        {
+            ScoreManager.gameOver = true; 
+        }
+        
+    }
+}
---
+++ Prototype1Runthrough2/Assets/Scripts/PlayerEnterTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//attach this to the player
+public class PlayerEnterTrigger : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerZone"))
+        {
+            ScoreManager.score++;
+        }
+    }
+        
+    
+}
