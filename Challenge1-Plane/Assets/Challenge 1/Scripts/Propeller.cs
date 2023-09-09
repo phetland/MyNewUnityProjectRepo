fadd40e40a87b7b45b0aa1ede9aa74f161452c93
phetland <46686189+phetland@users.noreply.github.com>
2023-09-09 11:43:12 -0500

    Challenge 1-Plane

---
+++ Challenge1-Plane/Assets/Challenge 1/Scripts/Propeller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+
+public class Propeller : MonoBehaviour
+
+{
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f,0f,speed*Time.fixedDeltaTime,Space.Self);
+    }
+}
